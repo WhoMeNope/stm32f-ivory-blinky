d830950a133e929cd52a6a626340cfbce42d2025
Martin Toman <whomenope@outlook.com>
2020-02-19 19:31:41 +0100

    Add Blink program

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,14 @@
 module Main where
 
-import Lib
+import Ivory.Tower.Config
+import Ivory.Tower.Options
+import Ivory.OS.FreeRTOS.Tower.STM32
+
+import SimpleBlink (app)
 
 main :: IO ()
-main = someFunc
+main = compileTowerSTM32FreeRTOS testplatform_stm32 p $
+  app testplatform_ledpin
+    where
+      p :: TOpts -> IO TestPlatform
+      p topts = getConfig topts testPlatformParser
