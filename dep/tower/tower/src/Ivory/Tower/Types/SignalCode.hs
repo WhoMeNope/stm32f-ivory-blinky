8341735f4adf5d676e7bcdb1234b63100f2c8867
Martin Toman <whomenope@outlook.com>
2020-02-19 19:32:16 +0100

    Add forked tower
    - Fix Semigroup -> Monoid

---
+++ dep/tower/tower/src/Ivory/Tower/Types/SignalCode.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE RankNTypes #-}
+
+{- | This module is a placeholder until we have backend-specific signal
+ - support.
+ -}
+module Ivory.Tower.Types.SignalCode where
+
+import Prelude ()
+import Prelude.Compat
+
+import qualified Data.Map as Map
+import Ivory.Language
+
+data SignalCode = SignalCode
+  { signalcode_signals :: Map.Map String GeneratedSignal
+  , signalcode_init    :: forall eff. Ivory eff ()
+  }
+
+instance Semigroup SignalCode where
+  a <> b = SignalCode
+    { signalcode_signals = signalcode_signals a `Map.union` signalcode_signals b
+    , signalcode_init    = signalcode_init a >> signalcode_init b
+    }
+
+instance Monoid SignalCode where
+  mempty = SignalCode
+    { signalcode_signals = Map.empty
+    , signalcode_init    = return ()
+    }
+
+newtype GeneratedSignal =
+  GeneratedSignal
+    { unGeneratedSignal :: (forall s . Ivory (AllocEffects s) ()) -> ModuleDef
+    -- ^ Unsafe signal continuation.
+    }
